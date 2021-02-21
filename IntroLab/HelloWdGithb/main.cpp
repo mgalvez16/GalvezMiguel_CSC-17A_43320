c7c5a90fb59a2940431ba0712ef9f8ff483f28b6
mgalvez16 <mikey.galvez1@gmail.com>
2021-02-20 16:51:05 -0800

    Signed-off-by: mgalvez16 <mikey.galvez1@gmail.com>

---
+++ IntroLab/HelloWdGithb/main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: Miguel Galvez
+ * Created on February 20, 2021, 4:40
+ * Purpose:  Hello World
+ * */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//Execution Begins Here
+int main() {
+    //Display Inputs/Outputs
+    cout<<"Hello World!"<<endl;
+    //Exit the Program - Cleanup
+    return 0;
+}
